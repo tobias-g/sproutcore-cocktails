e6bf74c739bd6aada249c95bfb1366c04d9feba4
Tobias Gray <tobias.gray@outlook.com>
2014-10-16 22:07:22 +0100

    Initial commit - created project and app, added README.md and .gitignore

---
+++ README.md
@@ -0,0 +1,13 @@
+# Folder6 Cocktails
+## Overview
+Written in Sproutcore the Folder6 cocktails app allows a user to view cocktails along with inputting an inventory to see what cocktails they can produce. Created by Tobias Gray as a medium to learn I've made the code public for others to learn from it too.
+
+## Related links
+[Folder6 website](http://www.folder6.com)
+
+[Walkthrough Turorial](http://www.folder6.com/articles/2014/10/sproutcore-cocktails-tutorial)
+
+[Sproutcore website](http://www.sproutcore.com)
+
+## Licence
+Do as you please with this source code fork, it make it better, learn from it its all under the [wtfpl licence](http://www.wtfpl.net/).
---
+++ apps/cocktails_app/main.js
@@ -0,0 +1,32 @@
+// ==========================================================================
+// Project:   CocktailsApp
+// Copyright: @2014 My Company, Inc.
+// ==========================================================================
+/*globals CocktailsApp */
+
+// This is the function that will start your app running.  The default
+// implementation will load any fixtures you have created then instantiate
+// your controllers and awake the elements on your page.
+//
+// As you develop your application you will probably want to override this.
+// See comments for some pointers on what to do next.
+//
+CocktailsApp.main = function main() {
+
+  // Step 1: Instantiate Your Views
+  // The default code here will make the mainPane for your application visible
+  // on screen.  If you app gets any level of complexity, you will probably
+  // create multiple pages and panes.
+  CocktailsApp.getPath('mainPage.mainPane').append() ;
+
+  // Step 2. Set the content property on your primary controller.
+  // This will make your app come alive!
+  //
+  // ex.
+  // var content = CocktailsApp.store.find(CocktailsApp.Group);
+  // CocktailsApp.groupsController.set('content', content);
+
+};
+
+
+function main() { CocktailsApp.main(); }
