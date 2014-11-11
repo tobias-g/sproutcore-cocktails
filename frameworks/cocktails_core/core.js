aedafe4a5c06a4d3e982a1c2d00eb814a27ae3a5
Tobias Gray <tobias.gray@outlook.com>
2014-11-11 14:37:55 +0000

    Added the cocktails_core framework that will hold the local storage datasource

---
+++ frameworks/cocktails_core/core.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   CocktailsCore
+// Copyright: @2014 Tobias Gray.
+// ==========================================================================
+/*globals CocktailsCore */
+
+/**
+ * The CocktailsCore framework houses the core building blocks
+ * of the cocktails application.
+ *
+ * @namespace
+ * @extends SC.Object
+ */
+CocktailsCore = SC.Object.create(
+    /** @scope CocktailsCore.prototype */ {
+
+    NAMESPACE: 'CocktailsCore',
+    VERSION: '0.1.0'
+});
---
+++ frameworks/cocktails_core/local_storage/datasource.js
@@ -0,0 +1,17 @@
+// ==========================================================================
+// Project:   CocktailsCore - local storage
+// Copyright: ©2014 Tobias Gray.
+// ==========================================================================
+/*globals CocktailsCore */
+
+/**
+ * TODO: describe
+ *
+ * @class
+ * @extends SC.DataSource
+ */
+CocktailsCore.LocalStorage = SC.DataSource.extend(
+    /** @scope CocktailsCore.LocalStorage.prototype */ {
+
+    // TODO: implement
+});
